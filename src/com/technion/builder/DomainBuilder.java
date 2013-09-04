34335ac6f8adec8313cf95f2aa17f996eab9a5d7
Sapir Golan <sapirgolan@gmail.com>
2013-09-04 17:10:28 +0300

    created buildAction()
    For now the method only writes actions's parameters

--- src/com/technion/builder/DomainBuilder.java
+++ src/com/technion/builder/DomainBuilder.java
@@ -1,9 +1,15 @@
 package com.technion.builder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 
+import com.technion.ai.dao.Action;
+import com.technion.ai.dao.Actions;
 import com.technion.ai.dao.Domain;
+import com.technion.ai.dao.Parameter;
 import com.technion.ai.dao.Predicat;
 import com.technion.utils.Utils;
 
@@ -29,6 +35,7 @@ public class DomainBuilder {
 			this.addEmptyLine();
 			buildPredicates();
 			this.addEmptyLine();
+			this.buldActions();
 		}
 		return this.stringBuilder;
 	}
@@ -43,6 +50,62 @@ public class DomainBuilder {
 		}
 		this.addStringInNewLine(")");
 	}
+	
+	private void buldActions() {
+		this.addEmptyLine();
+		String actionPrefix = prop.getProperty("actionPrefix");
+		String preconditionPrefix = prop.getProperty("preconditionPrefix");
+		String effectPrefix = prop.getProperty("effectPrefix");
+		
+		List<Action> actions = problemDomain.getActions().getAction();
+		for (Action action : actions) {
+			this.addStringInNewLine(actionPrefix);
+			this.addStringInLine(action.getName());
+			this.addStringInNewLine(preconditionPrefix);
+			List<Predicat> predicats = action.getPrecondition().getPredicat();
+			writeActionParameters(predicats);
+		}
+	}
+
+	private void writeActionParameters(List<Predicat> predicats) {
+		String parametersPrefix = prop.getProperty("parametersPrefix");
+		List<Parameter> parameters = new ArrayList<Parameter>();
+		
+		//collect all the parameters of all predicates
+		for (Predicat predicat : predicats) {
+			parameters.addAll( predicat.getParameter() );
+		}
+		
+		//create MAP. Key: parameters type; value: parameters name of this type
+		HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+		for (Parameter parameter : parameters) {
+			String paramName = parameter.getName();
+			String paramType = parameter.getType();
+			//if  we don't have any parameter of this type yet create a new entry for the type
+			//otherwise add the name to already existing type
+			if ( !map.containsKey(paramType) )
+			{
+				ArrayList<String> parametersNames = new ArrayList<String>( Arrays.asList(paramName) );
+				map.put(paramType, parametersNames);
+			} else {
+				map.get(paramType).add(paramName);
+			}
+		}
+		
+		this.addStringInNewLine(parametersPrefix);
+
+		//iterate over the map and prints its content
+		for (String type : map.keySet()) {
+			List<String> list = map.get(type);
+			for (String name : list) {
+				this.addStringInLine(name);
+			}
+			this.addStringInLine("- " + type);
+		}
+		this.addStringInLine(")");
+		
+		
+	}
 
 	private void buildTypes() {
 		String typesLine = prop.getProperty("types");
@@ -53,6 +116,10 @@ public class DomainBuilder {
 
 	private void addStringInNewLine(String newLine) {
 		stringBuilder.append("\n" + newLine);
+	}
+	
+	private void addStringInLine(String text) {
+		stringBuilder.append(" " + text);
 	}
 	
 	private void addEmptyLine() {
--- src/com/technion/junits/DomainBuilderTest.java
+++ src/com/technion/junits/DomainBuilderTest.java
@@ -1,14 +1,21 @@
 package com.technion.junits;
 
 import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.technion.ai.dao.Action;
+import com.technion.ai.dao.Actions;
 import com.technion.ai.dao.Domain;
 import com.technion.ai.dao.Parameter;
+import com.technion.ai.dao.Precondition;
 import com.technion.ai.dao.Predicat;
 import com.technion.ai.dao.Predicates;
 import com.technion.ai.dao.Types;
@@ -23,6 +30,7 @@ public class DomainBuilderTest {
 		Domain problemDomain = new Domain();
 		problemDomain.setName("junit");
 		problemDomain.setTypes(new Types());
+		problemDomain.setActions(new Actions());
 		problemDomain.setPredicates(new Predicates());
 		classUnderTest = new DomainBuilder(problemDomain); 
 	}
@@ -61,17 +69,9 @@ public class DomainBuilderTest {
 		
 		Domain problemDomain = classUnderTest.getProblemDomain();
 		problemDomain.setTypes(types);
-		Predicat predicat = new Predicat();
-		predicat.setName("predicat_1");
-		//add parameter
-		Parameter parameterOne = new Parameter();
-		parameterOne.setName("?x");
-		parameterOne.setType("type_1");
-		Parameter parameterTwo = new Parameter();
-		parameterTwo.setName("?z");
-		parameterTwo.setType("type_2");
-		predicat.getParameter().add(parameterOne);
-		predicat.getParameter().add(parameterTwo);
+		Parameter parameterOne = createParmeter("?x", "type_1");
+		Parameter parameterTwo = createParmeter("?z", "type_2");
+		Predicat predicat = createPredicat(Arrays.asList(parameterOne, parameterTwo));
 		//add predicat
 		problemDomain.getPredicates().getPredicat().add(predicat);
 		
@@ -82,4 +82,54 @@ public class DomainBuilderTest {
 		Assert.assertTrue("", domainStr.contains( "( predicat_1 ?x - type_1 ?z - type_2 )" ));
 	}
 
+	private Predicat createPredicat(List<Parameter> parameters) {
+		Predicat predicat = new Predicat();
+		predicat.setName("predicat_1");
+		//add parameter
+		for (Parameter parameter : parameters) {
+			predicat.getParameter().add(parameter);
+		}
+		return predicat;
+	}
+
+	private Parameter createParmeter(String name, String type) {
+		Parameter parameterOne = new Parameter();
+		parameterOne.setName(name);
+		parameterOne.setType(type);
+		return parameterOne;
+	}
+
+	@Test
+	public void testBuldActions() {
+		Actions actions = classUnderTest.getProblemDomain().getActions();
+		Action action = new Action();
+		action.setName("fix");
+		actions.getAction().add(action);
+		
+		Precondition precondition = new Precondition();
+		action.setPrecondition(precondition);
+		
+		Parameter parameterOne = createParmeter("?w", "type_1");
+		Parameter parameterTwo = createParmeter("?x", "type_1");
+		
+		Parameter parameterThree = createParmeter("?y", "type_2");
+		Parameter parameterFour = createParmeter("?z", "type_1");
+		
+		Parameter parameterFive = createParmeter("?a", "type_2");
+		
+		Predicat predicatOne = createPredicat(Arrays.asList(parameterOne, parameterTwo));
+		Predicat predicatTwo = createPredicat(Arrays.asList(parameterThree, parameterFour));
+		Predicat predicatThree = createPredicat(Arrays.asList(parameterFive));
+		precondition.getPredicat().add(predicatOne);
+		precondition.getPredicat().add(predicatTwo);
+		precondition.getPredicat().add(predicatThree);
+		
+		StringBuilder buildDomain = classUnderTest.buildDomain();
+		String domainStr = buildDomain.toString();
+		Assert.assertTrue("", domainStr.contains("(:action fix"));
+		Assert.assertTrue("", domainStr.contains( ":parameters(" ));
+		Assert.assertTrue("", domainStr.contains( "?w ?x ?z - type_1" ));
+		Assert.assertTrue("", domainStr.contains( "?y ?a - type_2" ));
+	}
 }
+
