b5ce67c0e1c6517f515ea162c0e310de056e087d
Sapir Golan <sapirgolan@gmail.com>
2013-09-18 14:00:52 +0300

    Added writeToFile() method

--- src/com/technion/utils/Utils.java
+++ src/com/technion/utils/Utils.java
@@ -1,5 +1,8 @@
 package com.technion.utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -19,5 +22,42 @@ public class Utils {
 			return null;
 		}
 		return prop;
+	}
+
+	public static void writeTextFile(String fileName, String fileContent)  {
+		BufferedWriter bufferWritter = null;
+		
+		if (fileName == null) {
+			fileName = "textFile.pddl";
+		}
+		File file = new File(fileName);
+
+		//Delete files it it already exists
+		if (file.exists()) {
+			file.delete();
+		}
+		try {
+			file.createNewFile();
+			//true = append file
+			FileWriter fileWritter = new FileWriter(file.getName(),true);
+			bufferWritter = new BufferedWriter(fileWritter);
+			bufferWritter.write(fileContent);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if (bufferWritter != null) {
+				try {
+					bufferWritter.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Done");
+		} //end finally
+
 	}
 }
